a4aea7dc813f4a6ee19c3c6a739fd457308df3b8
Sylvia <steffisylvia04@gmail.com>
2025-05-17 01:08:36 +0700

    add animation from about page

--- src/components/TechnologyItem.jsx
+++ src/components/TechnologyItem.jsx
@@ -1,6 +1,6 @@
-const TechnologyItem = ({ name, icon, desc, className }) => {
+const TechnologyItem = ({ name, icon, desc, className, animateType, animateOffset, animateDuration }) => {
   return (
-    <div className={`${className}`}>
+    <div className={`${className}`} data-aos={animateType} data-aos-offset={animateOffset} data-aos-duration={animateDuration}>
       <div className="bg-primary-20 rounded-xl p-4 w-full justify-items-center">
         <img src={icon} className="mb-4" alt="" />
         <p className="font-poppins text-body-xl-medium mb-1">{name}</p>

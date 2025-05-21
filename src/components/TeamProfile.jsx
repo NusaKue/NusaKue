ae645aa75a2dbd5e389593c116cc6d71f72a3b82
Sylvia <steffisylvia04@gmail.com>
2025-05-21 23:47:54 +0700

    make our team section responsive on about us page

--- src/components/TeamProfile.jsx
+++ src/components/TeamProfile.jsx
@@ -8,21 +8,21 @@ const TeamProfile = ({
 }) => {
   return (
     <div
-      className="flex flex-row lg:flex-col items-center justify-between gap-4 w-full h-auto py-4 rounded-xl"
+      className="flex flex-row lg:flex-col items-center gap-4 w-full lg:w-fit text-center"
       data-aos={animateType}
       data-aos-offset={animateOffset}
       data-aos-duration={animateDuration}
     >
       <img
         src={icon}
-        className="w-16 h-16 sm:w-40 sm:h-40 lg:w-60 lg:h-60 mb-4"
+        className="w-24 h-24 sm:w-44 sm:h-44 lg:w-60 lg:h-60"
         alt={name}
       />
-      <div className="flex-1 text-left lg:text-center ">
+      <div className="w-full lg:w-fit">
         <p className="font-poppins text-body-lg-medium lg:text-body-xl-medium mb-1 text-primary-100">
           {name}
         </p>
-        <p className="font-poppins text-body-sm-regular lg:text-body-md-regular text-primary-100">
+        <p className="font-poppins text-body-md-regular lg:text-body-lg-regular text-primary-100">
           {role}
         </p>
       </div>
